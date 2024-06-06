1b2c6841bcd4ff8ff8649658fc5201fce1b5bbe8
Setko AI <setko@msn.com>
2024-06-06 17:23:36 +0300

    Решение задачи № 1

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1058d8d22c48ecc1d8ac6923cae185f1c1254932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29795075e72dbd185aee79e4dc51676e5f3c4fa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
