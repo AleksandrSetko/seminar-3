1058d8d22c48ecc1d8ac6923cae185f1c1254932
Setko AI <setko@msn.com>
2024-06-06 16:17:17 +0300

    Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача № 1 Семинар 3
+// Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [10,90].
+
+// Начальные условия:
+
+// int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+// Выводится: 5
+
+int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3};
+int count = 0;
+foreach (int number in array)
+{
+    if (number >= 10 && number <= 90)
+    {
+        count++;
+    }
+    return count;
+}
+
+
+
+// Пример разворота числа
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int reversalNum = 0;
+// while (num != 0)
+// {
+// reversalNum = reversalNum * 10 + num % 10;
+// num /= 10;
+// }
+// Console.WriteLine(reversalNum);
--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655f040d988af1796ba4636656a085d136705038")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
